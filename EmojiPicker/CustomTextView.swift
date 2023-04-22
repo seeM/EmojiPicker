b478d2e26dd320260fe7e42a34fa734146172290
seem <mwlorgat@gmail.com>
2023-04-22 15:32:14 +0200

    Fixed!

--- EmojiPicker/CustomTextView.swift
+++ EmojiPicker/CustomTextView.swift
@@ -13,12 +13,18 @@ protocol CustomTextViewDelegate: AnyObject {
 
 class CustomTextView: NSTextView {
     weak var customDelegate: CustomTextViewDelegate?
+    var shouldResignFirstResponder = true
     
     override func keyDown(with event: NSEvent) {
         super.keyDown(with: event)
         if event.keyCode == 41 && event.modifierFlags.intersection(.deviceIndependentFlagsMask) == .shift {
+            shouldResignFirstResponder = false
             customDelegate?.showPicker(self)
+            shouldResignFirstResponder = true
         }
     }
+    
+    override func resignFirstResponder() -> Bool {
+        shouldResignFirstResponder && super.resignFirstResponder()
+    }
 }
-
